14319c694089ecef1670d388f2858a43fc031ca4
Connor Brinkley <connor.f.brinkley@gmail.com>
2025-06-01 20:48:07 -0500

    Create boxplot of smoker to cost data
    Using the matplotlib library I created a boxplot to represent the data
    that compares the cost of insurance for smokers to non-smokers

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ df = pd.read_csv('insurance.csv')
 # Step 2: Compare average insurance charges between smokers and non-smokers
 avg_charges = df.groupby('smoker')['charges'].mean()
 print("Average charges:\n", avg_charges)
+
+print("\nCounts by smoker status:\n", df['smoker'].value_counts())
+
+# Step 3: Visualize with a boxplot
+sns.boxplot(x='smoker', y='charges', data=df)
+plt.title('Insurance Charges by Smoking Status')
+plt.xlabel('Smoker')
+plt.ylabel('Charges')
+plt.show()
