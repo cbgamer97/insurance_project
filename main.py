2d9e39aa0986559aa2b89dcd8eb2f0fdf1457187
Connor Brinkley <connor.f.brinkley@gmail.com>
2025-06-01 20:32:53 -0500

    Import tools and load dataset

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import csv
+import matplotlib
+
+matplotlib.use('TkAgg')  # Or 'QtAgg' if you have Qt installed
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# Step 1: Load the dataset
+df = pd.read_csv('insurance.csv')
+
