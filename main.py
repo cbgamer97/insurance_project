ee6c022cd593ba6a71e4376d7742106be82afcdb
Connor Brinkley <connor.f.brinkley@gmail.com>
2025-06-01 20:34:47 -0500

    Calculate mean of smokers to higher or lower costs

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ import seaborn as sns
 # Step 1: Load the dataset
 df = pd.read_csv('insurance.csv')
 
+# Step 2: Compare average insurance charges between smokers and non-smokers
+avg_charges = df.groupby('smoker')['charges'].mean()
+print("Average charges:\n", avg_charges)
